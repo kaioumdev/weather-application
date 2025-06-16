3393788736534ce85fe35a8baf4ccb02e09ef7b7
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-17 00:16:57 +0600

    feat(search): create new search component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Header = () => {
     return (
         <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
             <nav className="container flex items-center justify-between py-6">
-                <a href="./index.html">
-                    <img className="h-9" src="./assets/logo.svg" alt="Weather App" />
-                </a>
+                <Logo></Logo>
 
                 <div className="flex items-center gap-4 relative">
                     <form action="#">
