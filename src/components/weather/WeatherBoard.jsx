f501dd21b631e479dc859a426d5558eb864a467c
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-17 10:51:54 +0600

    create weatherheadline component

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddToFavourite from './AddToFavourite'
 
 const WeatherBoard = () => {
     return (
@@ -6,28 +7,8 @@ const WeatherBoard = () => {
             <div
                 className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
                 <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-                    <div className="md:col-span-2">
-                        <div className="flex items-center justify-end space-x-6">
-                            <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
-                                <span>Add to Favourite</span>
-                                <img src="./assets/heart.svg" alt="" />
-                            </button>
+                    <AddToFavourite></AddToFavourite>
 
-                        </div>
-                    </div>
-                    <div>
-                        <div className="max-md:flex items-center justify-between md:-mt-10">
-                            <img src="./assets/cloud.svg" alt="cloud" />
-                            <div className="max-md:flex items-center max-md:space-x-4">
-                                <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">16°</h1>
-                                <div className="flex items-center space-x-4 md:mb-4">
-                                    <img src="./assets/pin.svg" />
-                                    <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
-                    </div>
                     <div>
                         <p className="text-sm lg:text-lg font-bold uppercase mb-8">thunderstorm with light drizzle</p>
                         <ul className="space-y-6 lg:space-y-6">
