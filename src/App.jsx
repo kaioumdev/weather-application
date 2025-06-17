0665218e0670c4d419f7d355f40daa12ff5cedfc
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-17 20:41:53 +0600

    successfully wrap app file by favouriteProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Header from "./components/header/Header"
 import WeatherBoard from "./components/weather/WeatherBoard"
 import WeatherProvider from './provider/WeatherProvider';
+import FavouriteProvider from './provider/FavouriteProvider';
 
 function App() {
   return (
     <WeatherProvider>
-      <div className="grid place-items-center h-screen">
-        <Header></Header>
-        <main>
-          <section>
-            <WeatherBoard></WeatherBoard>
-          </section>
-        </main>
-      </div>
+      <FavouriteProvider>
+        <div className="grid place-items-center h-screen">
+          <Header></Header>
+          <main>
+            <section>
+              <WeatherBoard></WeatherBoard>
+            </section>
+          </main>
+        </div>
+      </FavouriteProvider>
     </WeatherProvider>
   )
 }
