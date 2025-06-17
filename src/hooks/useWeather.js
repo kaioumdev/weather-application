378d7314c93a2dabecb4e0a02c884e64cc8ed348
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-17 16:33:15 +0600

    feat(weather-api): fetching weather api successfully

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -15,10 +15,48 @@ const useWeather = () => {
         latitude: '',
 
     });
-    const [isLoading, setIsLoading] = useState({
+    const [loading, setLoading] = useState({
         state: false,
         message: ""
     });
     const [error, setError] = useState(null);
+    const fetchWeatherData = async (latitude, longitude) => {
+        try {
+            setLoading({
+                ...loading,
+                state: true,
+                message: "Fetching weather data..."
+            });
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`);
+            if (!response.ok) {
+                const errorMessage = `Fetching weather data Failed: ${response.status}`;
+                throw new Error(errorMessage);
+            };
+            const data = await response.json();
+            const updateWeatherData = {
+                ...weatherData,
+                location: data?.name,
+                climate: data?.weather[0]?.main,
+                temperature: data.main.temp,
+                maxTemperature: data.main.temp_max,
+                minTemperature: data.main.temp_min,
+                humidity: data.main.humidity,
+                cloudPercentage: data.clouds.all,
+                wind: data.wind.speed,
+                time: date.dt,
+                longitude: longitude,
+                latitude: latitude
+            }
+            setWeatherData(updateWeatherData);
 
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading({
+                ...loading,
+                state: false,
+                message: ""
+            })
+        }
+    }
 }
