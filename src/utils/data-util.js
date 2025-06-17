2f69469ec67d9095699f57c0c90204c7e1722eb4
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-17 18:51:47 +0600

    complete date functiont to got the date

---
+++ src/utils/data-util.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+function getFormattedDate(value, type, inMS) {
+    if (!type) return value;
+
+    if (!inMS) {
+        value = value * 1000; // Convert to milliseconds if not already
+    };
+
+    const date = new Date(value);
+
+    let options;
+    if (type === 'date') {
+        options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    } else if (type === 'time') {
+        options = { hour: 'numeric', minute: 'numeric' };
+    }
+
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+}
