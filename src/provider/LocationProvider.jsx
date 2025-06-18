760fa7db2c01834247e6411da39dd4ba025046af
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-18 10:46:27 +0600

    create new locationProvider componet

---
+++ src/provider/LocationProvider.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const LocationProvider = ({ children }) => {
+    const [selectedLocation, setSelectedLocation] = useState({ location: "", latitude: 0, longitude: 0 });
+    return (
+        <LocationContext.Provider value={{ selectedLocation, setSelectedLocation }}>
+            {children}
+        </LocationContext.Provider>
+    )
+}
+
+export default LocationProvider
